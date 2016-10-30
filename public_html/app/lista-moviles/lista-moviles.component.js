42a9c49f04342116193b955c38cd60d40d83b0ac
Ruben Soler <r.solerginer@gmail.com>
2016-10-30 18:41:05 +0100

    select OK

--- public_html/app/lista-moviles/lista-moviles.component.js
+++ public_html/app/lista-moviles/lista-moviles.component.js
@@ -2,13 +2,13 @@
 
 angular.module('listaMoviles').component('listaMoviles',{
     templateUrl:'lista-moviles/lista-moviles.html',
-    controller: function($http){
+    controller: ['$http' ,function listaMovilesController($http){
         var self = this;
-
+        self.orden ="nombre";
         $http.get('lista-moviles/moviles.json').then(function(response){
            self.moviles=response.data;
         });
 
         self.orden ="fecha";
-    }
+    }]
 });
