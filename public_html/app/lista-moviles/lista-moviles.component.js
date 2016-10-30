763eaee6135cd8bf493de6ff237bff520b99d6c7
Ruben Soler <r.solerginer@gmail.com>
2016-10-30 21:42:42 +0100

    cambiar vistas OK

--- public_html/app/lista-moviles/lista-moviles.component.js
+++ public_html/app/lista-moviles/lista-moviles.component.js
@@ -5,6 +5,7 @@ angular.module('listaMoviles').component('listaMoviles',{
     controller: ['$http' ,function listaMovilesController($http){
         var self = this;
         self.orden ="nombre";
+        self.vista='movil--lista';
         $http.get('lista-moviles/moviles.json').then(function(response){
            self.moviles=response.data;
         });
