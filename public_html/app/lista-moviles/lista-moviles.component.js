b66f9ef0cc997383067bfe2bb209612330d68ee3
Ruben Soler <r.solerginer@gmail.com>
2016-10-30 17:26:07 +0100

    ng-selected

--- public_html/app/lista-moviles/lista-moviles.component.js
+++ public_html/app/lista-moviles/lista-moviles.component.js
@@ -8,21 +8,7 @@ angular.module('listaMoviles').component('listaMoviles',{
         $http.get('lista-moviles/moviles.json').then(function(response){
            self.moviles=response.data;
         });
- //        this.moviles = [
- //       {
- //         nombre: 'AAA Nexus S',
- //         info: 'Esto es un Nexus 5',
- //         fecha: timestamp
- //       }, {
- //         nombre: 'Motorola XOOM™ with Wi-Fi',
- //         info: 'Esto es un Motorola',
-  //        fecha: timestamp
-  ///      }, {
-  //        nombre: 'MOTOROLA XOOM™',
-  //        info: 'Esto es otro motorola',
-    //      fecha: timestamp
-   //     }
-   //   ];
+
         self.orden ="fecha";
     }
 });
