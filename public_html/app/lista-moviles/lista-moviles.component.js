90c85261bea6f06c622795ae67686dcad92b36a0
Ruben Soler <r.solerginer@gmail.com>
2016-10-30 17:12:37 +0100

    dependency injection y http

--- public_html/app/lista-moviles/lista-moviles.component.js
+++ public_html/app/lista-moviles/lista-moviles.component.js
@@ -2,22 +2,27 @@
 
 angular.module('listaMoviles').component('listaMoviles',{
     templateUrl:'lista-moviles/lista-moviles.html',
-    controller: function(){
-         this.moviles = [
-        {
-          nombre: 'AAA Nexus S',
-          info: 'Esto es un Nexus 5',
-          fecha: new Date(2016,2,3)
-        }, {
-          nombre: 'Motorola XOOM™ with Wi-Fi',
-          info: 'Esto es un Motorola',
-          fecha: new Date(2016,2,2)
-        }, {
-          nombre: 'MOTOROLA XOOM™',
-          info: 'Esto es otro motorola',
-          fecha: new Date(2016,2,1)
-        }
-      ];
-        this.orden ="fecha";
+    controller: function($http){
+        var self = this;
+
+        $http.get('lista-moviles/moviles.json').then(function(response){
+           self.moviles=response.data;
+        });
+ //        this.moviles = [
+ //       {
+ //         nombre: 'AAA Nexus S',
+ //         info: 'Esto es un Nexus 5',
+ //         fecha: new Date(2016,2,3)
+ //       }, {
+ //         nombre: 'Motorola XOOM™ with Wi-Fi',
+ //         info: 'Esto es un Motorola',
+  //        fecha: new Date(2016,2,2)
+  ///      }, {
+  //        nombre: 'MOTOROLA XOOM™',
+  //        info: 'Esto es otro motorola',
+    //      fecha: new Date(2016,2,1)
+   //     }
+   //   ];
+        self.orden ="fecha";
     }
 });
